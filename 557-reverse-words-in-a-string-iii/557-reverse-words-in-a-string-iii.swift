69686b8007cfac5d343d09836adfd3b1ffb65487
YuJeanCho <eugenie8@kakao.com>
2022-07-16 11:35:26 +0900

    Time: 251 ms (61.94%), Space: 14.9 MB (85.47%) - LeetHub

---
+++ 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.swift
@@ -0,0 +1,16 @@
+class Solution {
+    func reverseWords(_ s: String) -> String {
+        var output = ""
+
+        let words = s.split(separator: " ")
+
+        for (index, word) in words.enumerated() {
+            var reversedWord = String(word.reversed())
+            if index != words.count-1 {
+                reversedWord += " "
+            }
+            output.insert(contentsOf: reversedWord, at: output.endIndex)
+        }
+        return output
+    }
+}
