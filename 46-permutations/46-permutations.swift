d48feb30d81e90ee687515162b2c6ba29794778b
YuJeanCho <eugenie8@kakao.com>
2022-07-23 19:36:30 +0900

    Time: 23 ms (15.82%), Space: 14.1 MB (86.48%) - LeetHub

---
+++ 46-permutations/46-permutations.swift
@@ -0,0 +1,21 @@
+class Solution {
+    func permute(_ nums: [Int]) -> [[Int]] {
+        var result = [[Int]]()
+        var nums = nums
+        recurse(0, &nums, &result)
+        return result
+    }
+    
+    func recurse(_ first: Int, _ nums: inout [Int], _ result: inout [[Int]]) {
+        if first == nums.count {
+            result.append(nums)
+            return
+        }
+        
+        for index in first..<nums.count {
+            nums.swapAt(first, index)
+            self.recurse(first+1, &nums, &result)
+            nums.swapAt(first, index)
+        }
+    }
+}
