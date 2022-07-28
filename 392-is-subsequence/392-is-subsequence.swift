28241607ebbf62c240726544ae198695ba2634b7
YuJeanCho <eugenie8@kakao.com>
2022-07-28 09:52:04 +0900

    Time: 6 ms (52.51%), Space: 14.1 MB (67.95%) - LeetHub

--- 392-is-subsequence/392-is-subsequence.swift
+++ 392-is-subsequence/392-is-subsequence.swift
@@ -1,23 +1,7 @@
 class Solution {
     func isSubsequence(_ s: String, _ t: String) -> Bool {
-        if t.count < s.count { return false }
-        if s == "" || s.isEmpty { return true }
-        if s == t { return true }
-
-        var index = 0
-        var sArray = Array(s)
-        
-        for tEL in t
-        {
-          if sArray[index] == tEL
-          {
-              index += 1
-              if index == sArray.count
-              {
-                  return true
-              }
-          }  
-        }
-        return false
+        var ars = Array(s)
+        for c in t where c == ars.first { ars.removeFirst() }
+        return ars.isEmpty
     }
 }
