79d6ff862dd4e99e443472bf334628753705c5e3
YuJeanCho <eugenie8@kakao.com>
2022-07-29 09:39:32 +0900

    Time: 15 ms (72.85%), Space: 15 MB (16.77%) - LeetHub

--- 206-reverse-linked-list/206-reverse-linked-list.swift
+++ 206-reverse-linked-list/206-reverse-linked-list.swift
@@ -10,15 +10,15 @@
  */
 class Solution {
     func reverseList(_ head: ListNode?) -> ListNode? {
-    return reverseListInternal(head, nil)            
+    return reverseListRecursive(head, nil)            
   }
                                 
-  func reverseListInternal(_ current: ListNode?, _ previous: ListNode?) -> ListNode? {
+  func reverseListRecursive(_ current: ListNode?, _ previous: ListNode?) -> ListNode? {
     if current == nil { return previous }
       
     var next = current!.next      
     current!.next = previous              
       
-    return reverseListInternal(next, current)      
+    return reverseListRecursive(next, current)      
   }
 }
