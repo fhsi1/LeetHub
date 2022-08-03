122fb8581467c36f91b780082eaf85dabc242503
YuJeanCho <eugenie8@kakao.com>
2022-08-03 09:59:25 +0900

    Time: 119 ms (88.63%), Space: 15.4 MB (5.80%) - LeetHub

---
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.swift
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public var val: Int
+ *     public var left: TreeNode?
+ *     public var right: TreeNode?
+ *     public init(_ val: Int) {
+ *         self.val = val
+ *         self.left = nil
+ *         self.right = nil
+ *     }
+ * }
+ */
+
+class Solution {
+    func lowestCommonAncestor(_ root: TreeNode?, _ p: TreeNode?, _ q: TreeNode?) -> TreeNode? {
+        guard let root = root, let q = q, let p = p else { return nil }
+        if p.val > root.val && q.val > root.val {
+            return lowestCommonAncestor(root.right, p, q)
+        } else if p.val < root.val && q.val < root.val {
+            return lowestCommonAncestor(root.left, p, q)
+        } else {
+            return root
+        }
+    }
+}
