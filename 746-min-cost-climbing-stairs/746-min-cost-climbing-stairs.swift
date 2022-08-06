00a44436dd3c7b7475d47a3acc608d07678f3f65
YuJeanCho <eugenie8@kakao.com>
2022-08-06 10:19:08 +0900

    Time: 46 ms (21.29%), Space: 14.1 MB (66.03%) - LeetHub

---
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.swift
@@ -0,0 +1,10 @@
+class Solution {
+    func minCostClimbingStairs(_ cost: [Int]) -> Int {
+        var n = cost.count
+        var cost = cost
+        for i in 2..<n {
+            cost[i] = min(cost[i - 1], cost[i - 2]) + cost[i]
+        }
+        return min(cost[n-1], cost[n-2])
+    }
+}
