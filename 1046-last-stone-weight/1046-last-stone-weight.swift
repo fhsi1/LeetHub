5f7a7f3a4f4b48eb13b3608d97faded02f71ea29
YuJeanCho <eugenie8@kakao.com>
2022-08-10 10:14:58 +0900

    Time: 26 ms (6.35%), Space: 14.2 MB (12.70%) - LeetHub

---
+++ 1046-last-stone-weight/1046-last-stone-weight.swift
@@ -0,0 +1,24 @@
+class Solution {
+    func lastStoneWeight(_ stones: [Int]) -> Int {
+        var stones = stones
+        if stones.count == 0 {
+            return 0
+        } else if stones.count == 1 {
+            return stones[0]
+        }
+        
+        stones.sort()
+        while stones.count > 1 {
+            print(stones)
+            let last = stones.removeLast()
+            let secondLast = stones.removeLast()
+            if last > secondLast {
+                stones.append(last - secondLast)
+            }
+            
+            stones.sort()
+        }
+        
+        return stones.count > 0 ? stones[0] : 0
+    }
+}
