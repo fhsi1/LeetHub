b2bb58675442c6f61f366178c07fc1682a4f5291
YuJeanCho <eugenie8@kakao.com>
2022-08-15 12:35:55 +0900

    Time: 19 ms (89.52%), Space: 14.3 MB (65.50%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.swift
+++ 13-roman-to-integer/13-roman-to-integer.swift
@@ -1,22 +1,27 @@
-class Solution:
-    def romanToInt(self, s: str) -> int:
-        
-        table={'M':1000,'CM':900,'D':500,'CD':400,'C':100,'XC':90,
-					 'L':50,'XL':40,'X':10,'IX':9,'V':5,'IV':4,'I':1}
-            
-        value=0
-        temp=''
-        cursor=0
-        while cursor<len(s): # length of input string
-			
-			# Check now character, now+1 character
-            if (cursor+1)!=len(s) and s[cursor]+s[cursor+1] in table:
-                value+=table[s[cursor]+s[cursor+1]]
-                cursor+=2
-            else:
-                value+=table[s[cursor]]
-                cursor+=1
-                
-            
-        
-        return value
+class Solution {
+    func romanToInt(_ s: String) -> Int {
+        let dict = ["I" : 1, "V": 5, "X" : 10, "L" : 50, "C" : 100, "D" : 500, "M" : 1000]
+    
+        var n = s.count - 1
+        let arr = s.map { String($0) }
+        var prev = 0
+        var sum = 0
+
+        while n >= 0 {
+            guard let current = dict[arr[n]] else {
+                      return sum
+            }
+
+            if prev > current && prev > 0 {
+                sum = sum - current
+            } else {
+                sum = sum + current
+            }
+
+            prev = current
+            n -= 1
+        }
+
+        return sum
+    }
+}
