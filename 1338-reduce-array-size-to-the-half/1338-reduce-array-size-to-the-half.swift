df4dd298fe89d3f5af5d59f93e1459a178390173
YuJeanCho <eugenie8@kakao.com>
2022-08-18 14:24:34 +0900

    Time: 969 ms (40.00%), Space: 23.4 MB (20.00%) - LeetHub

---
+++ 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.swift
@@ -0,0 +1,23 @@
+class Solution {
+    func minSetSize(_ arr: [Int]) -> Int {
+        var dict = [Int: Int]()
+        
+        for val in arr {
+            dict[val] = (dict[val] ?? 0) + 1
+        }
+        
+        let sortedDict = dict.sorted { $0.value > $1.value}
+        var count = 0
+        var sum = 0
+        for ( _ , v) in sortedDict {
+            
+            if (arr.count / 2) > sum {
+                count += 1
+            } else {
+                return count
+            }
+            sum += v
+        }
+        return count
+    }
+}
