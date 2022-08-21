e0059e8bd4f01fee5caebfc6dd4edd4757260859
YuJeanCho <eugenie8@kakao.com>
2022-08-21 11:09:25 +0900

    Time: 26 ms (75.42%), Space: 14 MB (73.45%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.swift
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.swift
@@ -1,13 +1,5 @@
 class Solution {
     func searchMatrix(_ matrix: [[Int]], _ target: Int) -> Bool {
-        var res: Bool = false
-        for i in 0..<matrix.count  {
-            for j in 0..<matrix[i].count {
-                if matrix[i][j] == target {
-                    res = true
-                }
-            }
-        }
-        return res
+        return matrix.flatMap{ $0 }.contains(target)
     }
 }
